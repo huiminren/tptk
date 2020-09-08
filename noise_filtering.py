311975eb386ccf936b275438b7a6e4550bfccd72
Huimin Ren <hren@wpi.edu>
2020-09-08 15:44:56 +0800

    Update HeuristicFilter
    Update HeuristicFilter which considering about the boundary.
    The original one will remove the point who is next to the outlier.

--- noise_filtering.py
+++ noise_filtering.py
@@ -12,6 +12,10 @@ class NoiseFilter:
 
 
 class HeuristicFilter(NoiseFilter):
+    """
+    Remove outlier if it is out of the max speed
+    """
+
     def __init__(self, max_speed):
         super(NoiseFilter, self).__init__()
         self.max_speed = max_speed
@@ -20,16 +24,35 @@ class HeuristicFilter(NoiseFilter):
         pt_list = traj.pt_list
         if len(pt_list) <= 1:
             return None
-        pre_pt = pt_list[0]
-        clean_pt_list = [pre_pt]
-        for cur_pt in pt_list[1:]:
-            time_span = (cur_pt.time - pre_pt.time).total_seconds()
-            dist = distance(pre_pt, cur_pt)
-            if time_span > 0 and dist / time_span <= self.max_speed:
-                clean_pt_list.append(cur_pt)
-                pre_pt = cur_pt
+
+        remove_inds = []
+        for i in range(1, len(pt_list) - 1):
+            time_span_pre = (pt_list[i].time - pt_list[i - 1].time).total_seconds()
+            dist_pre = haversine_distance(pt_list[i - 1], pt_list[i])
+            time_span_next = (pt_list[i + 1].time - pt_list[i].time).total_seconds()
+            dist_next = haversine_distance(pt_list[i], pt_list[i + 1])
+            speed_pre = dist_pre / time_span_pre
+            speed_next = dist_next / time_span_next
+            # the first point is outlier
+            if i == 1 and speed_pre > self.max_speed > speed_next:
+                remove_inds.append(0)
+                
+            # the last point is outlier
+            elif i == len(pt_list) - 2 and speed_next > self.max_speed >= speed_pre:
+                remove_inds.append(len(pt_list) - 1)
+                
+            # middle point is outlier
+            elif speed_pre > self.max_speed and speed_next > self.max_speed:
+                remove_inds.append(i)
+
+        clean_pt_list = []
+        for j in range(len(pt_list)):
+            if j in remove_inds:
+                continue
+            clean_pt_list.append(pt_list[j])
+
         if len(clean_pt_list) > 1:
-            return Trajectory(traj.oid, self.get_tid(traj.oid, clean_pt_list), clean_pt_list)
+            return Trajectory(traj.oid, traj.did, self.get_tid(traj.oid, clean_pt_list), clean_pt_list)
         else:
             return None
 
